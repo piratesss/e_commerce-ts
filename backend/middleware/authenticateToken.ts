6d1da1a6e404bc10d707d9cee9606159a12ebca0
shubham-tam <tamrakarshubham23@gmail.com>
2024-07-09 23:14:03 +0545

    feat: auth token added

---
+++ backend/middleware/authenticateToken.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req: any, res: any, next: any) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        req.user = user;
+
+        const newToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '15m',
+        });
+        res.setHeader('Authorization', `Bearer ${newToken}`);
+
+        next();
+    });
+};
---
+++ backend/utils/token.ts
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken';
+
+export const generateAccessToken = (user: {}) => {
+    return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1m' });
+};
