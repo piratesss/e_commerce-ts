e94a9a8eafe8f42adbf22fdf9eacbcf3710cdba4
shubham-tam <tamrakarshubham23@gmail.com>
2024-07-09 23:47:53 +0545

    feat: updated jwt access token to 15m

--- backend/utils/token.ts
+++ backend/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
 
 export const generateAccessToken = (user: {}) => {
-    return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1m' });
+    return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '15m' });
 };
