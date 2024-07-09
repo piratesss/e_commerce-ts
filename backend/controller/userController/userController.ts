73628ff870a50ac370aae241674a6c46a1ac1f16
shubham-tam <tamrakarshubham23@gmail.com>
2024-07-09 20:50:05 +0545

    feat: added interface for user

--- backend/controller/userController/userController.ts
+++ backend/controller/userController/userController.ts
@@ -1,25 +1,24 @@
 import expressAsyncHandler from 'express-async-handler';
-
 import { getAllUsersService } from './userService';
 
 const getAllUsers = expressAsyncHandler(async (req: any, res: any) => {
-    try {
-        const [allUsers, allUsersError]: any = await getAllUsersService();
+    const [allUsers, allUsersError] = await getAllUsersService();
 
-        if (allUsersError) {
-            console.error('Error fetching users:', allUsersError);
-            return res.status(500).send('Error fetching users');
-        }
+    if (allUsersError) {
+        console.error('Error fetching users:', allUsersError);
+        return res.status(500).send('Error fetching users');
+    }
 
-        if (Array.isArray(allUsers?.rows)) {
-            return res.status(200).json(allUsers?.rows);
-        } else {
-            console.error('Unexpected result: allUsers is not an array');
-            return res.status(500).send('Unexpected result');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+    if (allUsers && Array.isArray(allUsers.rows)) {
+        const usersWithoutPassword = allUsers.rows.map(user => {
+            const { password, ...userWithoutPassword } = user;
+            return userWithoutPassword;
+        });
+
+        return res.status(200).json(usersWithoutPassword);
+    } else {
+        console.error('Unexpected result: allUsers is not an array');
+        return res.status(500).send('Unexpected result');
     }
 });
 
--- backend/controller/userController/userService.ts
+++ backend/controller/userController/userService.ts
@@ -1,14 +1,31 @@
 import pool from '../../config/db';
 
-const getAllUsersService = async () => {
-    const getAllUsers = await pool.query(`SELECT * FROM "user"`);
-    if (getAllUsers?.rows?.length) {
-        return [getAllUsers, null];
-    }
+interface User {
+    id: string;
+    first_name: string;
+    email: string;
+    address: string;
+    phone_number: string;
+    payment_mode: string | null;
+    middle_name: string;
+    last_name: string;
+    password: string;
+    image_id: string | null;
+}
+
+interface GetAllUsersResponse {
+    rows: User[];
+}
+
+type GetAllUsersServiceResult = [GetAllUsersResponse, null] | [null, string];
+
+const getAllUsersService = async (): Promise<GetAllUsersServiceResult> => {
     try {
+        const getAllUsers = await pool.query(`SELECT * FROM "user"`);
+        return [{ rows: getAllUsers.rows }, null];
     } catch (error) {
         console.error('Error getting user details:', error);
-        return [null, error];
+        return [null, error.message];
     }
 };
 
